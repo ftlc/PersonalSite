bba40081df814685bc608cbf592dbcce7141bbe9
Harry Sadoyan <harrysadoyan@gmail.com>
2021-04-19 19:04:40 -0400

    Remove registration endpoints

--- lib/harry_web/router.ex
+++ lib/harry_web/router.ex
@@ -43,8 +43,6 @@ defmodule HarryWeb.Router do
   scope "/", HarryWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
-    get "/users/register", UserRegistrationController, :new
-    post "/users/register", UserRegistrationController, :create
     get "/users/log_in", UserSessionController, :new
     post "/users/log_in", UserSessionController, :create
     get "/users/reset_password", UserResetPasswordController, :new
